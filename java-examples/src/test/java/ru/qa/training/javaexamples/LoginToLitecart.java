82d1cabf7d8e8838130a8e80abe6ba9f8de110cc
AleksandraTokareva <ittokareva@gmail.com>
2017-11-30 08:30:17 -0600

    created new project LoginToLitecart

---
+++ java-examples/src/test/java/ru/qa/training/javaexamples/LoginToLitecart.java
@@ -0,0 +1,41 @@
+package ru.qa.training.javaexamples;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class LoginToLitecart {
+    private WebDriver driver;
+    private WebDriverWait wait;
+
+    @Before
+    public void start() {
+        System.setProperty("webdriver.chrome.driver", "C:\\Tools\\chromedriver_win32\\chromedriver.exe");
+        driver = new ChromeDriver();
+        wait = new WebDriverWait(driver, 10);
+    }
+
+
+    @Test
+    public void myFirstTest() {
+
+        driver.get("http://localhost/litecart/admin/");
+        driver.findElement(By.name("username")).sendKeys("admin");
+        driver.findElement(By.name("password")).sendKeys("admin");
+        driver.findElement(By.name("remember_me")).click();
+        driver.findElement(By.name("login")).click();
+
+
+    }
+
+    @After
+    public void stop() {
+        driver.quit();
+        driver = null;
+    }
+}
+

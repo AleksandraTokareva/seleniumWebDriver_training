a496eb6d40cc5c25f45c6f40a6fcd64fc8c48770
AleksandraTokareva <ittokareva@gmail.com>
2017-12-10 07:48:32 -0600

    added changes to MYSecondTest and MyStickerTest

--- java-examples/src/test/java/ru/qa/training/javaexamples/MyStickerTest.java
+++ java-examples/src/test/java/ru/qa/training/javaexamples/MyStickerTest.java
@@ -5,44 +5,25 @@ import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.By;
-import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.support.ui.WebDriverWait;
-
-import static org.openqa.selenium.support.ui.ExpectedConditions.presenceOfElementLocated;
-
 import java.util.List;
 import java.util.concurrent.TimeUnit;
 
 
 public class MyStickerTest {
     private WebDriver driver;
-    private WebDriverWait wait;
-    private WebElement elementwait;
 
-    boolean isElementPresent(WebDriver driver, By locator) {
-        try {
-            driver.findElement(locator);
-            return true;
-        } catch (NoSuchElementException ex) {
-            return false;
-        }
-    }
 
     @Before
     public void start() {
         driver = new ChromeDriver();
-        //wait = new WebDriverWait(driver, 10);
-        //elementwait = wait.until(presenceOfElementLocated(By.cssSelector("name")));
     }
-
 
     @Test
     public void myStickerTest() {
         driver.get("http://localhost/litecart");
-        //List<WebElement> stickers = driver.findElements(By.cssSelector("img.image")).findElement(By.cssSelector("sticker"));
         List<WebElement> items = driver.findElements(By.cssSelector("li.product.column.shadow.hover-light"));
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
         for (int i = 0; i < items.size(); i++) {
@@ -50,17 +31,13 @@ public class MyStickerTest {
             List<WebElement> stickers = items.get(i).findElements(By.cssSelector("div.sticker"));
             Assert.assertTrue(stickers.size() == 1);
 
-
         }
     }
-
 
     @After
     public void stop() {
         driver.quit();
         driver = null;
-
-
     }
 }
 

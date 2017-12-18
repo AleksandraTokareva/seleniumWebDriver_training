0fa7a6349bf866628ade7e63c517248542d42120
AleksandraTokareva <ittokareva@gmail.com>
2017-12-17 22:58:50 -0600

    updated CreateNewUser and created CreateNewUserOld

--- java-examples/src/test/java/ru/qa/training/javaexamples/CreateNewUserOld.java
+++ java-examples/src/test/java/ru/qa/training/javaexamples/CreateNewUserOld.java
@@ -9,7 +9,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.Select;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
-public class CreateNewUser {
+public class CreateNewUserOld {
     private WebDriver driver;
     private WebDriverWait wait;
 
@@ -21,16 +21,6 @@ public class CreateNewUser {
 
     @Test
     public void newUser() {
-        createAccount();
-        driver.findElement(By.linkText("Logout")).click();
-        driver.findElement(By.name("email")).sendKeys("tester4@test.com");
-        driver.findElement(By.name("password")).sendKeys("tester7001");
-        driver.findElement(By.name("remember_me")).click();
-        driver.findElement(By.name("login")).click();
-        driver.findElement(By.linkText("Logout")).click();
-    }
-
-    public void createAccount() {
         driver.get("http://localhost/litecart/en/");
         driver.findElement(By.linkText("New customers click here")).click();
         driver.findElement(By.name("firstname")).sendKeys("James");
@@ -40,17 +30,26 @@ public class CreateNewUser {
         driver.findElement(By.name("city")).sendKeys("Irving");
         Select select = new Select(driver.findElement(By.tagName("select")));
         select.selectByValue("US");
-        driver.findElement(By.name("email")).sendKeys("tester4@test.com");
+        Select select1 = new Select(driver.findElement(By.xpath("//tr[5]/td[2]/select")));
+        //select1.selectByValue("TX");
+        select1.selectByVisibleText("Texas");
+        driver.findElement(By.name("email")).sendKeys("tester2@test.com");
         driver.findElement(By.name("phone")).sendKeys("+15128034896");
         driver.findElement(By.name("password")).sendKeys("tester7001");
         driver.findElement(By.name("confirmed_password")).sendKeys("tester7001");
-        driver.findElement(By.name("create_account")).click();
+       driver.findElement(By.name("create_account")).click();
+        driver.findElement(By.linkText("Logout")).click();
+       driver.findElement(By.name("email")).sendKeys("tester2@test.com");
+       driver.findElement(By.name("password")).sendKeys("tester7001");
+        driver.findElement(By.name("remember_me")).click();
+        driver.findElement(By.name("login")).click();
+       driver.findElement(By.linkText("Logout")).click();
     }
 
     @After
     public void stop() {
-        driver.quit();
-        driver = null;
-    }
+       driver.quit();
+       driver = null;
+   }
 }
 

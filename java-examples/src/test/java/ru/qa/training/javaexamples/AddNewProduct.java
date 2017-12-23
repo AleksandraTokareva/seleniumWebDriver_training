6320159471ba76227f29431b44910c5e3665482d
AleksandraTokareva <ittokareva@gmail.com>
2017-12-22 20:43:34 -0600

    updated AddNewProduct

--- java-examples/src/test/java/ru/qa/training/javaexamples/AddNewProduct.java
+++ java-examples/src/test/java/ru/qa/training/javaexamples/AddNewProduct.java
@@ -12,11 +12,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.Select;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
-import javax.xml.bind.Element;
-import java.util.ArrayList;
-import java.util.Collection;
 import java.util.List;
-import java.util.concurrent.TimeUnit;
 
 
 public class AddNewProduct {
@@ -40,11 +36,7 @@ public class AddNewProduct {
 
     @Test
     public void addNewProduct() {
-        driver.get("http://localhost/litecart/admin/?app=catalog&doc=catalog");
-        driver.findElement(By.name("username")).sendKeys("admin");
-        driver.findElement(By.name("password")).sendKeys("admin");
-        driver.findElement(By.name("remember_me")).click();
-        driver.findElement(By.name("login")).click();
+        login();
         List<WebElement> listButton = driver.findElements(By.cssSelector("a.button"));
         for (int i = 1; i < listButton.size(); i++) {
             listButton.get(i).click();
@@ -66,21 +58,15 @@ public class AddNewProduct {
                 categories.get(j).click();
             }
         }
-        // Select selectCategory = new Select(driver.findElement(By.tagName("select")));
-        //selectCategory.selectByValue("1");
-        driver.findElement(By.name("quantity")).sendKeys("30");
-        //Select selectSold = new Select(driver.findElement(By.tagName("select")));
-        //selectSold.selectByValue("2");
 
+        driver.findElement(By.name("quantity")).sendKeys("30");
         String filePath = System.getProperty("user.dir") + "/src/test/resources/white_duck.png";
         driver.findElement(By.name("new_images[]")).sendKeys(filePath);
-        // driver.findElement(By.name("save")).click();
         driver.findElement(By.linkText("Information")).click();
         Select selectManufacturer = new Select(driver.findElement(By.name("manufacturer_id")));
         selectManufacturer.selectByValue("1");
         driver.findElement(By.name("short_description[en]")).sendKeys("White Duck Toy");
         driver.findElement(By.cssSelector("div.trumbowyg-editor")).sendKeys("White Duck Toy");
-        //driver.findElement(By.name("save")).click();
         driver.findElement(By.linkText("Prices")).click();
         driver.findElement(By.name("purchase_price")).sendKeys("1");
         driver.findElement(By.name("prices[USD]")).sendKeys("20.0000");
@@ -88,25 +74,31 @@ public class AddNewProduct {
         driver.get("http://localhost/litecart/admin/?app=catalog&doc=catalog");
         driver.findElement(By.linkText("Rubber Ducks")).click();
         List<WebElement> imageList = driver.findElements(By.xpath("//td[3]/a"));
-           // Assert.assertTrue(isElementPresent(By.linkText("White Duck")));
-            Assert.assertTrue(verifyProduct(imageList, "White Duck"));
+        Assert.assertTrue(isElementPresent(By.linkText("White Duck")));
 
-        }
+    }
 
+    public void login() {
+        driver.get("http://localhost/litecart/admin/?app=catalog&doc=catalog");
+        driver.findElement(By.name("username")).sendKeys("admin");
+        driver.findElement(By.name("password")).sendKeys("admin");
+        driver.findElement(By.name("remember_me")).click();
+        driver.findElement(By.name("login")).click();
+    }
 
-    public Boolean verifyProduct(List<WebElement> list,String checkedText) {
-        for(int i=0;i<list.size();i++){
-            if(list.get(i).getText().equals("checkedText")){
+    public Boolean verifyProduct(List<WebElement> list, String checkedText) {
+        for (int i = 0; i < list.size(); i++) {
+            if (list.get(i).getText().equals("checkedText")) {
                 return true;
             }
         }
         return false;
     }
+
+    @After
+    public void stop() {
+        driver.quit();
+        driver = null;
+    }
 }
-// @After
-// public void stop() {
-//driver.quit();
-// driver = null;
-// }
-//}
 
